f8cb7abf03592d46cc8c9152639e0c1886de008b
Frederique Corgier <e2364446@cmaisonneuve.qc.ca>
2024-05-15 17:24:01 -0400

    Ajout du code pour exo1

--- exo1.py
+++ exo1.py
@@ -1,1 +1,19 @@
+import json
+import csv
 
+# Fonction de conversion
+def complexe_en_csv(nombre_complexe):
+    return [nombre_complexe.real, nombre_complexe.imag]
+
+# Création du fichier json
+with open('data.json', 'r') as fichier_json:
+    data = json.load(fichier_json)
+
+# Conversion nombres complexes en csv
+data_csv = complexe_en_csv(complex(nombre_complexe[0], nombre_complexe[1])) for nombre_complexe in data]
+
+# Écriture des données
+with open('output.csv', 'w', newline='') as fichier_csv:
+    writer = csv.writer(fichier_csv)
+    writer.writerow(['réel', 'imaginaire']) # En-tête
+    writer.writerows(data_csv)
